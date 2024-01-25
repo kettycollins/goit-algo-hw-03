68a7fff7cc20a0df759b3bed69d6e70d06703f68
kettycollins <144057221+kettycollins@users.noreply.github.com>
2024-01-25 17:46:50 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -8,17 +8,19 @@ def koch_curve(t, order, size):
             koch_curve(t, order - 1, size / 3)
             t.left(angle)
 
-def draw_koch_curve(order, size=300):
+def draw_koch_snowflake(order, size=300):
     window = turtle.Screen()
     window.bgcolor("white")
 
     t = turtle.Turtle()
-    t.speed(0)  
+    t.speed(0)
     t.penup()
-    t.goto(-size / 2, -size / 2 / 3**0.5)
+    t.goto(-size / 2, size / 2 / 3**0.5)
     t.pendown()
 
-    koch_curve(t, order, size)
+    for _ in range(3):
+        koch_curve(t, order, size)
+        t.right(120)
 
     window.mainloop()
 
@@ -30,7 +32,7 @@ def main():
             raise ValueError("Рівень рекурсії повинен бути не від'ємним числом.")
         
         # Виклик функції з введеним рівнем рекурсії
-        draw_koch_curve(level)
+        draw_koch_snowflake(level)
     except ValueError as e:
         print(f"Помилка: {e}")
 
